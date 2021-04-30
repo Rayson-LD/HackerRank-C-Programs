cb5b965cbff05d02ee6250a670142a72c3ed05e8
Rayson L Dsz <72185590+Rayson-LD@users.noreply.github.com>
2021-04-30 21:03:00 +0530

    Update hackerRank.c

--- hackerRank.c
+++ hackerRank.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
 
+/*1*/
+int main()
+{
+    char s[100] = "Welcome to C programming.";
+    printf("Hello, World!\n");
+   
+    printf("%s",s);
+    return 0;
+    
+}
