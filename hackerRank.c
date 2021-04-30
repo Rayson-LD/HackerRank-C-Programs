8585ee07703bd7783a7d952d764b3bff54c559ca
Rayson L Dsz <72185590+Rayson-LD@users.noreply.github.com>
2021-04-30 21:05:01 +0530

    Update hackerRank.c

--- hackerRank.c
+++ hackerRank.c
@@ -40,3 +40,24 @@ printf("%s",sen);
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
     return 0;
 }
+
+/*3*/
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+	int a,b,s1,m1;
+    float c,d,s2,m2;
+    scanf("%d %d",&a,&b);
+    scanf("%f %f",&c,&d);
+    s1 = a + b;
+    m1 = a - b;
+    s2 = c + d;
+    m2 = c - d;
+    printf("%d %d\n",s1,m1);
+    printf("%0.1f %0.1f",s2,m2);
+    return 0;
+}
