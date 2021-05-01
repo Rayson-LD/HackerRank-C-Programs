91191c29aa9d8dc5d9ffb3b79ff7f71ca1ff643d
Rayson L Dsz <72185590+Rayson-LD@users.noreply.github.com>
2021-05-01 16:18:22 +0530

    Update hackerRank.c

--- hackerRank.c
+++ hackerRank.c
@@ -254,3 +254,46 @@ int main() {
     return 0;
 }
 
+/*9*/
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+//Complete the following function.
+
+
+void calculate_the_maximum(int n, int k) {
+  int maxA = 0;
+  int maxO = 0;
+  int maxX = 0;
+  for(int i=1;i<=n;i++)
+  {
+      for(int j=i+1;j<=n;j++)
+      {
+          if((i&j)>maxA && (i&j)<k)
+          {
+              maxA = i&j;
+          }
+          if((i|j)>maxO && (i|j)<k)
+          {
+              maxO = i|j;
+          }
+          if((i^j)>maxX && (i^j)<k)
+          {
+              maxX = i^j;
+          }
+      }
+  }
+  printf("%d\n%d\n%d",maxA,maxO,maxX);
+}
+
+int main() {
+    int n, k;
+  
+    scanf("%d %d", &n, &k);
+    calculate_the_maximum(n, k);
+    
+ 
+    return 0;
+}
+
