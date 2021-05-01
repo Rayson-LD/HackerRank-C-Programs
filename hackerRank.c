159cefff53580b04cdbcac0a189ecfd83920826d
Rayson L Dsz <72185590+Rayson-LD@users.noreply.github.com>
2021-05-01 16:17:28 +0530

    Update hackerRank.c

--- hackerRank.c
+++ hackerRank.c
@@ -233,3 +233,24 @@ int main()
 
     return 0;
 }
+
+/*8*/
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+	
+    int n;
+    int sum = 0;
+    scanf("%d", &n);
+    while(n>0)
+    {
+        sum = sum + n%10;
+        n = n/10;
+    }
+    printf("%d",sum);
+    return 0;
+}
+
