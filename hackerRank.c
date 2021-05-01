bd9f6b0aa68a397b656d9fa1fd1d00bc49034f38
Rayson L Dsz <72185590+Rayson-LD@users.noreply.github.com>
2021-05-01 16:23:03 +0530

    Update hackerRank.c

--- hackerRank.c
+++ hackerRank.c
@@ -375,3 +375,24 @@ int main()
         printf("%d ", *(arr + i));
     return 0;
 }
+
+/*13*/
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    char *s;
+    s = malloc(1024 * sizeof(char));
+    scanf("%[^\n]", s);
+    s = realloc(s, strlen(s) + 1);
+    for (char *c = s; *c != NULL; c++) {
+    if (*c == ' ') {
+        *c = '\n';
+    }
+}
+printf("%s", s);
+    return 0;
+}
