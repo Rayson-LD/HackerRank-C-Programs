c78576ff86fc1a4f0f33888be99f1c0abfee121b
Rayson L Dsz <72185590+Rayson-LD@users.noreply.github.com>
2021-05-01 16:24:54 +0530

    Update hackerRank.c

--- hackerRank.c
+++ hackerRank.c
@@ -417,3 +417,14 @@ for(temp=0;temp<10;temp++){
 }  
     return 0;
 }
+
+/*15*/
+int marks_summation(int* marks, int number_of_students, char gender) {
+ 
+    int sum = 0;
+    for (int i = (gender != 'b' ? 1 : 0); i < number_of_students; i = i + 2) 
+        sum += *(marks + i);
+    
+    return sum;
+}
+
